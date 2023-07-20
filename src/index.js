cd47edce779c91a6b3d1f566e2e01a6457c4c750
HlobisileLukhele <111096303+HlobisileLukhele@users.noreply.github.com>
2023-07-20 04:11:03 +0000

    imported jquery cdn linked

--- src/index.js
+++ src/index.js
@@ -5,3 +5,24 @@ const toggleButton = document.getElementById("toggle-button");
 toggleButton.addEventListener("click", function() {
   navbar.classList.toggle("show");
 });
+
+//Implementing the Slick Slider
+
+$('.slider').slick ({
+  centerMode: true,
+  dots:true,
+  autoplay:true,
+  centerPadding: '60px',
+  slidesShow: 3,
+  responsive: [{
+    breakpoints : 768,
+    settings: {
+      arrows: false,
+      centerMode: true,
+      centerPadding: '40px',
+      Infinity: true,
+      slidesShow:1
+    }
+  }]
+
+})
