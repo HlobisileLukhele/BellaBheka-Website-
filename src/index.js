7c3a41b5bcf45b0c25f33c142611631bdc90b79c
HlobisileLukhele <111096303+HlobisileLukhele@users.noreply.github.com>
2023-07-24 13:33:08 +0000

    created a function to call the owl carousel

--- src/index.js
+++ src/index.js
@@ -6,23 +6,10 @@ toggleButton.addEventListener("click", function() {
   navbar.classList.toggle("show");
 });
 
-//Implementing the Slick Slider
+//Implementing the slider carousel
 
-$('.slider').slick ({
-  centerMode: true,
-  dots:true,
-  autoplay:true,
-  centerPadding: '60px',
-  slidesShow: 3,
-  responsive: [{
-    breakpoints : 768,
-    settings: {
-      arrows: false,
-      centerMode: true,
-      centerPadding: '40px',
-      Infinity: true,
-      slidesShow:1
-    }
-  }]
-
-})
+$(document).ready(function() {
+ 
+  $(".owl-carousel").owlCarousel();
+ 
+});
