2998309733862bd7dd9f5259e795477b3240aa56
HlobisileLukhele <111096303+HlobisileLukhele@users.noreply.github.com>
2023-07-25 15:32:21 +0000

    set the id for the owl carousel

--- src/index.js
+++ src/index.js
@@ -10,51 +10,14 @@ toggleButton.addEventListener("click", function() {
 
 $(document).ready(function() {
  
-  $(".owl-carousel").owlCarousel();
+  $("#owl-demo").owlCarousel({
+ 
+      autoPlay: 4000, //Set AutoPlay to 3 seconds
+ 
+      items : 8,
+      itemsDesktop : [1199,3],
+      itemsDesktopSmall : [979,3]
+ 
+  });
  
 });
-
-$(".owl-carousel").owlCarousel({
- 
-  //Basic Speeds
-  slideSpeed : 200,
-  paginationSpeed : 800,
-
-  //Autoplay
-  autoPlay : false,
-  goToFirst : true,
-  goToFirstSpeed : 1000,
-
-  // Navigation
-  navigation : false,
-  navigationText : ["prev","next"],
-  pagination : true,
-  paginationNumbers: true,
-
-  // Responsive
-  responsive: true,
-  items : 5,
-  itemsDesktop : [1199,4],
-  itemsDesktopSmall : [980,3],
-  itemsTablet: [768,2],
-  itemsMobile : [479,1]
-
-})
-
-//calling the plugin 
-$(".owl-carousel").owlCarousel()
- 
-//get carousel instance data and store it in variable owl
-var owl = $(".owl-carousel").data('owlCarousel');
- 
-//Public methods
-owl.next()   
-owl.prev()   
-owl.goTo(x)  
- 
-owl.update() 
- 
-owl.buildControlls()    
-owl.destroyControlls()  
- 
-owl.play() // Autoplay
