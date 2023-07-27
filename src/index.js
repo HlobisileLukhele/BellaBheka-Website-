e7378db8cdb11381cb0e62e14cc1e5bc3258d349
Hlobisle Lukhele <n.thwala03@gmail.com>
2023-07-27 02:01:14 -0700

    deleted node modules package folder

--- src/index.js
+++ src/index.js
@@ -6,15 +6,25 @@ toggleButton.addEventListener("click", function() {
   navbar.classList.toggle("show");
 });
 
+<<<<<<< Updated upstream
 //Implementing the slider carousel
 
+=======
+//Implementing the Slick Slider
+>>>>>>> Stashed changes
 $(document).ready(function() {
  
   $("#owl-demo").owlCarousel({
  
+<<<<<<< Updated upstream
       autoPlay: 4000, //Set AutoPlay to 3 seconds
  
       items : 8,
+=======
+      autoPlay: 3000, //Set AutoPlay to 3 seconds
+ 
+      items : 4,
+>>>>>>> Stashed changes
       itemsDesktop : [1199,3],
       itemsDesktopSmall : [979,3]
  
