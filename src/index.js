6a1c58d9139ce2805e334d959226c4cf77215922
Hlobisle Lukhele <n.thwala03@gmail.com>
2023-07-29 02:37:08 -0700

    changed the number of images to 9

--- src/index.js
+++ src/index.js
@@ -14,9 +14,9 @@ $(document).ready(function() {
  
       autoPlay: 3000, //Set AutoPlay to 3 seconds
  
-      items : 4,
-      itemsDesktop : [1199,3],
-      itemsDesktopSmall : [979,3]
+      items : 9,
+      itemsDesktop : [1199,9],
+      itemsDesktopSmall : [979,9]
  
   });
  
