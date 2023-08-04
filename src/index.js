bb60645a6e534ed804cb8494f88049d3162b1815
HlobisileLukhele <111096303+HlobisileLukhele@users.noreply.github.com>
2023-08-04 07:48:25 -0700

    deleted the slide up animation function and styles

--- src/index.js
+++ src/index.js
@@ -70,46 +70,5 @@ window.addEventListener('load', () => {
 
 countUp();
 
-//animating the services cards
-
-function isElementInViewport(el) {
-  var rect = el.getBoundingClientRect();
-  return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-  );
-}
-
-function addSlideUpAnimation(entries, observer) {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add('slide-up');
-      observer.unobserve(entry.target);
-    }
-  });
-}
-
-
-const options = {
-  root: null,
-  rootMargin: '0px',
-  threshold: 0.2,
-};
-
-// Apply slide-up animation on desktop devices only
-if (window.matchMedia('(min-width: 768px)').matches) {
-  // Select all cards with the class 'col-sm'
-  const cards = document.querySelectorAll('.col-sm');
-
-  // Create an Intersection Observer instance
-  const observer = new IntersectionObserver(addSlideUpAnimation, options);
-
-  // Observe each card to check when it is in the viewport
-  cards.forEach(card => observer.observe(card));
-}
-
-  
 
 
